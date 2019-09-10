13640987766906e5d2e9b1a30d9da27f96270d08
Bram de Wit <b19m98@gmail.com>
2019-09-10 23:05:13 +0200

    total delay NS
    sum of all current delays, so if a delayed train passes several stations it is counted several times. which means it is the total waiting time for customers of the NS

--- main.py
+++ main.py
@@ -31,5 +31,63 @@ def main():
     #print(ns_api.get_departures("8400057"))
 
 
+    list_disruptions = ns_api.get_disruptions()
+    list_train_stations = ns_api.get_train_stations()
+
+    delay_total = 0
+
+    for station in list_train_stations:
+        station_id = station['id']
+        print(station_id)
+
+        list_departures = ns_api.get_departures(station_id)
+
+        for departures in list_departures :
+            delay = departures['delay_seconds']
+            delay_total += delay
+
+
+    print("de totale delay van alle treinen met vertraging op alle stations opgeteld is ", delay_total)
+    
+    
+    list_departures_from_AmsterdamCS = ns_api.get_departures("8400058") #id van amsterdam centraal 
+    delay_total = 0
+
+    for departures in list_departures_from_AmsterdamCS:
+        
+        delay = departures['delay_seconds']
+        delay_total += delay 
+
+    print("de totale delay op amsterdam centraal station", delay_total)   
+
+
+
+    impact_buffer = 0 
+    stations_size = 0
+
+    for disruptions in list_disruptions:
+
+       impact = disruptions['impact']
+       stations = disruptions['stations']
+
+#this sadly only works when there is only one instance of the highest impact nr 
+#if there are several highest impact numbers then it will settle on the first it encounters 
+       if len(stations) > stations_size:
+           stations_size = len(stations)
+
+       if impact > int(impact_buffer):
+           impact_buffer = impact
+           station_buffer = stations
+
+
+       #print(impact)
+       #print(stations, len(stations))
+       
+         
+
+    print("\rthe highest impact nr. : ", impact_buffer )
+    print("\r these stations were impacted by this disruption :", station_buffer)
+    print("\r the most stations affected by a single disruption :", stations_size)
+
 if __name__ == "__main__":
     main()
